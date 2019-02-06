aec43e54cbb94fe44f52e6bfc7af4a1e8f3a3e5f
Phil Crump <phil@philcrump.co.uk>
2019-02-06 23:01:23 +0000

    /nb/: Formatting, remove chat & log functions.

--- nb/websdr-base.js
+++ nb/websdr-base.js
@@ -69,8 +69,14 @@ var passbandobjstart=0;    // position (in pixels) of start of passband on frequ
 var passbandobjwidth=0;    // width of passband in pixels
 var centerfreq=bandinfo[band].centerfreq;
 
+function guidGenerator() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
 
-
+var user_id = guidGenerator();
 
 function debug(a)
 {
@@ -124,7 +130,7 @@ function send_soundsettings_to_server()
         +"&lo="+lo
         +"&hi="+hi
         +"&mode="+m
-        +"&name="+encodeURIComponent(document.usernameform.username.value) 
+        +"&name="+user_id
         );
   } catch (e) {};
   timeout_idle_restart()
@@ -1222,14 +1228,6 @@ function registerTouchEvents(id, touchStart, touchMove) {
    elem.addEventListener('touchend', touchEnd);
 }
 
-function setusernamecookie() {
-   createCookie('username',document.usernameform.username.value,365*5);
-   var p=document.getElementById("please1");
-   if (p) p.innerHTML="Your name or callsign: ";
-   p=document.getElementById("please2");
-   if (p) p.innerHTML="";
-   send_soundsettings_to_server();
-}
 
 //----------------------------------------------------------------------------------------
 // things related to interaction with the mouse (clicking & dragging on the frequency axes)
@@ -1540,20 +1538,6 @@ window.onkeydown = keydown;
 //----------------------------------------------------------------------------------------
 // functions that create part of the HTML GUI
 
-function document_username() 
-{
-  var x= readCookie('username');
-  if (x) {
-    document.write('<a id="please">Your name or callsign: ');
-    document.write('<input type="text" value="" name="username" onchange="setusernamecookie()" onclick=""></a>');
-    document.usernameform.username.value=x;
-  } else {
-    document.write('<a id="please"><span id="please1"><b><i>Please log in by typing your name or callsign here (it will be saved for later visits in a cookie):<\/i><\/b></span> ');
-    document.write('<input type="text" value="" name="username" onchange="setusernamecookie()" onclick=""></a>');
-  }
-}
-
-
 function document_waterfalls() 
 {
   if (view==Views.allbands || view==Views.othersslow) nwaterfalls=nvbands;
@@ -1699,80 +1683,3 @@ function record_click()
       record_start();
    }
 }
-
-
-
-//----------------------------------------------------------------------------------------
-// things not directly related to the SDR: chatbox, logbook
-
-function sendchat()
-{
-  timeout_idle_restart()
-  var xmlHttp;
-  try { xmlHttp=new XMLHttpRequest(); }
-    catch (e) { try { xmlHttp=new ActiveXObject("Msxml2.XMLHTTP"); }
-      catch (e) { try { xmlHttp=new ActiveXObject("Microsoft.XMLHTTP"); }
-        catch (e) { alert("Your browser does not support AJAX!"); return false; } } }
-  var url="/~~chat";
-  var msg=encodeURIComponent(document.chatform.chat.value);
-  url=url+"?name="+encodeURIComponent(document.usernameform.username.value)+"&msg="+encodeURIComponent(document.chatform.chat.value);
-  xmlHttp.open("GET",url,true);
-  xmlHttp.send(null);
-  document.chatform.chat.value="";
-  return false;
-}
-
-function chatnewline(s)
-// called by updates fetched from the server
-{
-  var o=document.getElementById('chatboxnew');
-  if (!o) return;
-  if (s[0]=='-') {
-     // remove line from chatbox
-     var div=document.createElement('div');
-     div.innerHTML=s;
-     s=div.innerHTML;
-     var re=new RegExp('<br>'+s.substring(1).replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")+'.*','g');
-     o.innerHTML=o.innerHTML.replace(re,'<br>');
-     return;
-  }
-  // add line to chatbox
-  o.innerHTML+='<br>'+s+'\n';
-  o.scrollTop=o.scrollHeight;
-}
-
-function sendlogclear()
-{
-  document.logform.comment.value="";
-}
-
-function sendlog()
-{
-  var xmlHttp;
-  try { xmlHttp=new XMLHttpRequest(); }
-    catch (e) { try { xmlHttp=new ActiveXObject("Msxml2.XMLHTTP"); }
-      catch (e) { try { xmlHttp=new ActiveXObject("Microsoft.XMLHTTP"); }
-        catch (e) { alert("Your browser does not support AJAX!"); return false; } } }
-  var url="/~~loginsert";
-  url=url
-     +"?name="+encodeURIComponent(document.usernameform.username.value)
-     +"&freq="+nominalfreq()
-     +"&call="+encodeURIComponent(document.logform.call.value)
-     +"&comment="+encodeURIComponent(document.logform.comment.value)
-     ;
-  xmlHttp.open("GET",url,true);
-  xmlHttp.send(null);
-  document.logform.call.value="";
-  document.logform.comment.value="";
-  xmlHttp.onreadystatechange=function()
-    {
-    if(xmlHttp.readyState==4)
-      {
-      document.logform.comment.value=xmlHttp.responseText;
-      }
-    }
-  setTimeout("document.logform.comment.value=''",1000);
-  return false;
-}
-
-
