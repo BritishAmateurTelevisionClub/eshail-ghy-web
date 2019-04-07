60c23c7d08ac0072931e207d0e56bfe98637bb66
Phil Crump <phil@philcrump.co.uk>
2019-04-07 16:44:51 +0000

    /nb/websdr-base.js: Remove guid for user id, not required, reduces ~~othersjj? ajax data volume.

--- nb/websdr-base.js
+++ nb/websdr-base.js
@@ -69,14 +69,7 @@ var passbandobjstart=0;    // position (in pixels) of start of passband on frequ
 var passbandobjwidth=0;    // width of passband in pixels
 var centerfreq=bandinfo[band].centerfreq;
 
-function guidGenerator() {
-    var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-}
-
-var user_id = guidGenerator();
+var user_id = '';
 
 function debug(a)
 {
